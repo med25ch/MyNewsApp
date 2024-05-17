0a1d0e0e9a90856bd2fef63b0ff3b9fcce58d9ef
med25 <m.chemmi25@gmail.com>
2024-05-17 10:34:05 -0400

    Commit : Refactor retrofit code

--- app/src/main/java/com/example/mynewsapp/retrofit/INewsApi.kt
+++ app/src/main/java/com/example/mynewsapp/retrofit/INewsApi.kt
@@ -1,11 +1,21 @@
 package com.example.mynewsapp.retrofit
 
 import com.example.mynewsapp.models.ArticlesResult
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface INewsApi {
 
-    @GET("/v2/top-headlines?country=us&apiKey=4247013acfde47c0bacb38754bdfdebd")
-    suspend fun getTopHeadlines() : Response<ArticlesResult>
+    @GET("top-headlines")
+    suspend fun getTopHeadlines(@Query("country") country : String, @Query("apiKey") apiKey : String
+    ) : Call<ArticlesResult>
+
+    @GET("top-headlines")
+    suspend fun getTopHeadlinesByCategory(
+        @Query("country") country : String,
+        @Query("apiKey") apiKey : String,
+        @Query("category") category : String,
+    ) : Call<ArticlesResult>
 }
